8331b2e5d12d325dc102f8667bafd1ecd1df41ca
arsh-dang <arshdang15@outlook.com>
2026-04-07 11:38:29 +1000

    feat: add CORS middleware to FastAPI

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,9 @@
 from app.routes.stations import app
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], # Allow frontend 
+    allow_methods=["*"], # Allow all HTTP methods
+    allow_headers=["*"], # Allow all headers
+)
